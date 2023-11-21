1e6857f2b61034cbb50f1ff1625bb23c2b83843e
IFalimendikov <falimendikov.ivan@gmail.com>
2023-11-21 17:28:24 +0100

    test

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -10,15 +9,10 @@ import (
 	"strconv"
 
 	"github.com/bwmarrin/discordgo"
-	"github.com/joho/godotenv"
 )
 
 func main() {
 
-	err := godotenv.Load()
-	if err != nil {
-	  log.Fatal("Error loading .env file")
-	}
 	token := os.Getenv("DISCORD_BOT_TOKEN")
 
 	port()
